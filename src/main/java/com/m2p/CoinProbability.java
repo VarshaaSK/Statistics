95310b5e2278da88805163fa1b0a9e6172582109
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-31 13:14:14 +0530

    Req 1 : Add. Included the Logic to check if Probablity of Head is Same as Tail

--- src/main/java/com/m2p/CoinProbability.java
+++ src/main/java/com/m2p/CoinProbability.java
@@ -1,4 +1,20 @@
 package com.m2p;
 
 public class CoinProbability {
+    private final double probability;
+    CoinProbability(double probability){
+        this.probability = probability;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if(this == object){
+            return true;
+        }
+        if(object == null || this.getClass() != object.getClass()){
+            return false;
+        }
+        CoinProbability that = (CoinProbability) object;
+        return probability == that.probability;
+    }
 }
--- src/test/java/com/m2p/CoinProbabilityTest.java
+++ src/test/java/com/m2p/CoinProbabilityTest.java
@@ -1,5 +1,42 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CoinProbabilityTest {
-    CoinProbability probability = new CoinProbability();
+
+
+    @Test
+    void toCheckIfProbabilityOfHeadsIsSameAsTail(){
+        CoinProbability HeadsProbability = new CoinProbability(0.5);
+        CoinProbability tailProbability = new CoinProbability(0.5);
+
+        assertThat(HeadsProbability , is(equalTo(tailProbability)));
+    }
+
+    @Test
+    void toCheckIfProbabilityOfHeadsIsNotSameAsTail(){
+        CoinProbability HeadsProbability = new CoinProbability(0.5);
+        CoinProbability tailProbability = new CoinProbability(0.16);
+
+        assertThat(HeadsProbability , is(not(equalTo(tailProbability))));
+    }
+
+    @Test
+    void toCheckIfProbabilityOfHeadsIsNotSameAsNull(){
+        CoinProbability HeadsProbability = new CoinProbability(0.5);
+
+        assertThat(HeadsProbability , is(not(equalTo(null))));
+    }
+
+    @Test
+    void comparingObjectToPrimitiveDataType(){
+        CoinProbability HeadsProbability = new CoinProbability(0.5);
+        double tailProbability = 0.5;
+        assertThat(HeadsProbability , is(not(equalTo(tailProbability))));
+    }
 }
